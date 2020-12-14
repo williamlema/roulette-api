ad8ca721048a92461e1bf54c72ee21a091c1e2ba
William Leon <weleonm@gmail.com>
2020-12-13 19:29:17 -0500

    Define resource to create roulette

--- src/main/java/com/masiv/roulette/adapter/in/controller/RouletteController.java
+++ src/main/java/com/masiv/roulette/adapter/in/controller/RouletteController.java
@@ -1,8 +1,20 @@
 package com.masiv.roulette.adapter.in.controller;
 
+import com.masiv.roulette.adapter.in.controller.dto.RouletteIdPayload;
+import com.masiv.roulette.usecase.service.RouletteService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 public class RouletteController {
 
+    private final RouletteService rouletteService;
+
+    @PostMapping
+    public ResponseEntity<RouletteIdPayload> create(){
+        return ResponseEntity.ok(rouletteService.create());
+    }
 }
